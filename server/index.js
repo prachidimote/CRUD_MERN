93045f7c1e23553b37c8192d88ee92232a7b6c43
prachidimote <81797753+prachidimote@users.noreply.github.com>
2024-05-06 17:28:15 +0530

    database connect

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
-
 import express from 'express';
-const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const PORT = 8000;
-app.listen(PORT, () => console.log(`server running successfully on port ${PORT}`));
+// connection();
+mongoose
+.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log('Connected to MongoDB!');
+})
+.catch((err) => {
+    console.log(err);
+})
+const app = express();
+app.use(express.json());
+app.listen(8000, () => console.log(`server running successfully on port ${PORT}`));
